1dfa4130ed2ba1132daed4931e83ddd0902e2b5c
pa_gorovoy <pa_gorovoy@bingo-boom.com>
2019-04-07 17:09:32 +0300

    small fix names

--- src/li-algoritm.js
+++ src/li-algoritm.js
@@ -28,53 +28,54 @@ const findPath = (ax, ay, bx, by) => {
     const startPoint = lab[ay][ax];
     const endPoint = lab[by][bx];
     let stop;
-    let d;
+    let step;
 
     if (startPoint === WALL || endPoint === WALL) {
         return 'Incorrect entry data';
     }
 
     lab[ay][ax] = 0;
-    d = 0;
+    step = 0;
+
     while (!stop && lab[by][bx] === BLANK) {
         stop = true;
 
         for (let y = 0; y < lab.length; y++) {
             for (let x = 0; x < lab[y].length; x++) {
-                if (lab[y][x] === d) {
+                if (lab[y][x] === step) {
                     for (let k = 0; k < 4; k++) {
                         const px = x + PX[k];
                         const py = y + PY[k];
                         if (px >= 0 && px < WEIGHT && py >= 0 && py < HEIGHT && lab[py][px] === BLANK) {
                             stop = false;
-                            lab[py][px] = d + 1;
+                            lab[py][px] = step + 1;
                         }
                     }
                 }
             }
         }
-        d++;
+        step++;
     }
 
     if (lab[by][bx] === BLANK) {
         return 0;
     }
-    const size = lab[by][bx];
+    const steps = lab[by][bx];
     let x = bx;
     let y = by;
-    d = size;
+    step = steps;
 
-    while (d > 0) {
+    while (step > 0) {
         path = [...path, [x, y]];
-        d--;
+        step--;
 
         for (let k = 0; k < 4; k++) {
-            const iy = y + PY[k];
-            const ix = x + PX[k];
+            const coordY = y + PY[k];
+            const coordX = x + PX[k];
 
-            if (ix >= 0 && ix < WEIGHT && iy >= 0 && iy < HEIGHT && lab[iy][ix] === d) {
-                x = x + PX[k];
-                y = y + PY[k];
+            if (coordX >= 0 && coordX < WEIGHT && coordY >= 0 && coordY < HEIGHT && lab[coordY][coordX] === step) {
+                x = coordX;
+                y = coordY;
                 break;
             }
         }
